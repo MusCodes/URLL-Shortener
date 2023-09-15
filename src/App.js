8dd88980f13c2b4a3789085c4af974e273c54fbd
Mustafa <MustafaGino1@yahoo.com>
2023-09-14 22:26:43 -0500

    Css style added for better design.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+
+import "./App.css"
+
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [input, setInput]= useState('')
+  const [message, setMessage] = useState("");
+  const [input, setInput] = useState("");
 
- const handleSubmit= () => {
- axios({
-    method: 'get',
-    url: `http://localhost:5052/api/shortnerURL/shortenURL`,
-    params: {longURL: input}
-   }).then(function (response){
-    setMessage(response.data)
-   }).catch((error) => {
-    console.log(" this is catch error", error);
-   })
+  const handleSubmit = () => {
+    axios({
+      method: "get",
+      url: `http://localhost:5052/api/shortnerURL/shortenURL`,
+      params: { longURL: input },
+    })
+      .then(function (response) {
+        setMessage(response.data);
+      })
+      .catch((error) => {
+        console.log(" this is catch error", error);
+      });
   };
- 
-  
 
   return (
-    <div className="UrlShortner">
-      <input value={input} onChange={(event) => setInput(event.target.value)} type='text' placeholder='longURL'></input>
-      <button onClick={handleSubmit}>   get url</button>
-      <h2>{message}</h2>
-    </div>
-  );}
+    <div className="main-box">
+      
+        <div className="input-box">
+          <input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+            type="text"
+            placeholder="longURL"
+            required
+          ></input>
+        </div>
 
+        <button onClick={handleSubmit}>
+          <span>
+            </span> 
+            <span>
+            </span>
+            <span>
+            </span>
+            <span>
+            </span>
+            get url</button>
+        <div className="input-box">
+          <a href={message} target="_blank">
+        <h2>{message}</h2>
+        </a>
+      
+        </div>
+    
+    </div>
+  );
+}
 
 export default App;
